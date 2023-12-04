a931c6d3d02bbbe5498699d4ace572e9283aec27
Matthew Coming <coming.matthew@gmail.com>
2023-12-04 01:05:47 -0600

    day 2: part 1 finished

---
+++ 2023/02_cube_conundrum/cube_conundrum_1.py
@@ -0,0 +1,75 @@
+# Matthew Coming
+# Advent of Code 2023
+
+
+# --- Day 2: Cube Conundrum ---
+
+# You're launched high into the atmosphere! The apex of your trajectory just barely reaches the surface of a large island floating in the sky. You gently land in a fluffy pile of leaves. It's quite cold, but you don't see much snow. An Elf runs over to greet you.
+
+# The Elf explains that you've arrived at Snow Island and apologizes for the lack of snow. He'll be happy to explain the situation, but it's a bit of a walk, so you have some time. They don't get many visitors up here; would you like to play a game in the meantime?
+
+# As you walk, the Elf shows you a small bag and some cubes which are either red, green, or blue. Each time you play this game, he will hide a secret number of cubes of each color in the bag, and your goal is to figure out information about the number of cubes.
+
+# To get information, once a bag has been loaded with cubes, the Elf will reach into the bag, grab a handful of random cubes, show them to you, and then put them back in the bag. He'll do this a few times per game.
+
+# You play several games and record the information from each game (your puzzle input). Each game is listed with its ID number (like the 11 in Game 11: ...) followed by a semicolon-separated list of subsets of cubes that were revealed from the bag (like 3 red, 5 green, 4 blue).
+
+# For example, the record of a few games might look like this:
+
+# Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+# Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+# Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+# Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+# Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+
+# In game 1, three sets of cubes are revealed from the bag (and then put back again). The first set is 3 blue cubes and 4 red cubes; the second set is 1 red cube, 2 green cubes, and 6 blue cubes; the third set is only 2 green cubes.
+
+# The Elf would first like to know which games would have been possible if the bag contained only 12 red cubes, 13 green cubes, and 14 blue cubes?
+
+# In the example above, games 1, 2, and 5 would have been possible if the bag had been loaded with that configuration. However, game 3 would have been impossible because at one point the Elf showed you 20 red cubes at once; similarly, game 4 would also have been impossible because the Elf showed you 15 blue cubes at once. If you add up the IDs of the games that would have been possible, you get 8.
+
+# Determine which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of the IDs of those games?
+
+red = "red"
+green = "green"
+blue = "blue"
+
+def get_possible_games(content: "list[str]", max_red: int, max_green: int, max_blue: int):
+    possible_games = []
+    for idx, line in enumerate(content):
+        if line.count(":") > 1: raise Exception("input is malformed, unexpected ':'")
+        # split meta data from game data
+        game_id_str, game = line.split(":")
+
+        # exctract game id
+        game_id = int(game_id_str.split(" ")[1])
+
+        # assume round is possible
+        possible = True
+        for round_ in game.split(";"):
+            # check assumption and break if impossible
+            colors = round_.split(",")
+            for color in colors:
+                color = color.strip()
+                number = int(color.split(" ")[0])
+            
+                if red in color and number > max_red: possible = False; break
+                if green in color and number > max_green: possible = False;  break
+                if blue in color and number > max_blue: possible = False;  break
+            
+            if not possible:
+                # game is impossible, don't need to check anymore colors in this round
+                break
+
+        if possible:
+            possible_games.append(game_id)
+
+    return possible_games
+
+if __name__ == "__main__":
+    # get lines of input
+    with open("input.txt") as f:
+        content = f.readlines()
+        game_ids = get_possible_games(content, max_red=12, max_green=13, max_blue=14)
+        print(sum(game_ids))
+    
